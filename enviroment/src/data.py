a77dfa9c9521c1c678414313d91c18d7a005fdc0
Oscar <odrusso1@gmail.com>
2018-05-24 08:30:46 +1200

    Update data

--- enviroment/src/data.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-class Data():
-    def __init__(self):
-        self.context_options = ["rectangle", "paddock", "pond", "playground", "pool", "park"]
-
-    def choose(self, full_list):
-        num = randint(0, len(full_list) - 1)
-        return full_list[num]
