c166c5d64568728379cfbea0eeb12ae0331a6c17
Oscar Russo <odrusso1@gmail.com>
2021-09-04 21:12:08 +1200

    Add more logic to the question page

--- web/src/components/Question.tsx
+++ web/src/components/Question.tsx
@@ -1,5 +1,60 @@
-import React from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
+
+// TODO Refactor this into a models area
+type QuestionTypeDTO = {
+    type: string,
+    id: string
+}
 
 export const Question = (): JSX.Element => {
-    return (<h1>Question page</h1>)
+
+    const listOfQuestions: QuestionTypeDTO[] = [
+        {type: "mcat", id: "1"},
+        {type: "mcat", id: "2"},
+        {type: "mcat", id: "3"},
+    ]
+
+    const [selectedQuestion, setSelectedQuestion] = useState<QuestionTypeDTO>(listOfQuestions[0])
+
+    // TODO: Think about this.
+    const seed = "12345"
+
+    // Fetch the question every time the selectedQuestion changes
+    useEffect(() => {
+        // TODO: Better (centralised) URL construction
+        const url = `/api/v1/questions/${selectedQuestion.type}/${selectedQuestion.id}?seed=${seed}`
+
+        // TODO: Actually fetch from the API
+        console.log(`fetching from URL: ${url}`)
+    }, [selectedQuestion])
+
+    return (
+        <div>
+            <h1>Question page</h1>
+            <QuestionPicker questions={listOfQuestions} setSelectedQuestion={setSelectedQuestion}/>
+            <p>current seed: {seed}</p>
+            <p>selected question: {selectedQuestion.type} {selectedQuestion.id}</p>
+        </div>
+    )
+}
+
+// TODO: Refactor the picker into a separate file
+type QuestionPickerProps = {
+    questions: QuestionTypeDTO[],
+    setSelectedQuestion: (q: QuestionTypeDTO) => void
+}
+
+const QuestionPicker = ({questions, setSelectedQuestion}: QuestionPickerProps): JSX.Element => {
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setSelectedQuestion(questions[e.target.value])
+    }
+
+    return (
+        <select onChange={handleChange}>
+            {questions.map((question, index) =>
+                <option key={index} value={index}>{question.type} {question.id}</option>
+            )}
+        </select>
+    )
 }
