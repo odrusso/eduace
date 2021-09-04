4ea8182d4026ccf4fe3f69a3d26c4821e2b25b95
Ashton Moore <ashton.chris.moore@gmail.com>
2021-09-04 19:17:17 +1200

    - Create base Question class - Improve question mapping - app.py as a controller - remove services class (for now)

--- app/app.py
+++ app/app.py
@@ -1,20 +1,17 @@
 from flask import Flask, request
-from .services import mcat
+from time import time
+from .questions import get_question
 
 app = Flask(__name__)
 
-QUESTION_TYPE_MAPPING = {
-    'mcat': mcat,
-}
-
 @app.route("/question/<question_type>/<question_id>", methods=["GET"])
-def question_router(question_type, question_id=None):
+def question_router(question_type, question_id):
 
-    if question_class := QUESTION_TYPE_MAPPING.get(question_type, ""):
-        seed = request.args.get("seed", "")
-        question = question_class(question_id, seed)
+    seed = request.args.get("seed", time())
+    question = get_question(question_type, question_id, seed)
 
-    return question
+    return question.__dict__
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/questions.py
+++ app/questions.py
@@ -1,28 +1,56 @@
-import sys
-from sympy import symbols, latex, Eq, Rational
+from sympy import symbols, latex, Eq
 
-QUESTIONS = {
-    'mcat': set(
-        '1',
-    ),
+class QuestionError:
+    def __init__(self):
+        self.description = "Question not found."
+
+
+class QuestionTypeError:
+    def __init__(self):
+        self.description = "Question type not found."
+
+
+class Question:
+    def __init__(self, seed):
+        self.description = ""
+        self.question = ""
+        self.seed = seed
+    
+    @property
+    def __dict__(self):
+        return {
+            "description": self.description,
+            "question": self.question,
+        }
+
+
+class MCATQuestion1(Question):
+    def __init__(self, seed):
+        super().__init__(seed)
+
+        x = symbols("x")
+        ## TODO: Use seed
+        a = 2
+        b = 3
+        self.description = "Solve a linear equation."
+        self.question = latex(Eq(a * x + b, 0))
+
+
+QUESTION_MAPPING = {
+    'mcat': {
+        '1': MCATQuestion1,
+    },
 }
 
-def get_question(question_id, question_type, seed):
-    """Mildy dodgy"""
-    return getattr(sys.modules[__name__], question_type + "_" + question_id)(seed)
 
+def get_question(question_type, question_id, seed):
 
-def mcat_1(seed):
-    x = symbols('x')
-    ## TODO: use the seed
-    a = 2
-    b = 3
+    if QUESTION_MAPPING.get(question_type, None):
+        if QUESTION_MAPPING.get(question_type).get(question_id, None):
+            question = QUESTION_MAPPING.get(question_type).get(question_id)(seed)
+        else:
+            question = QuestionError()
+    else:
+        question = QuestionTypeError()
 
-    question = Eq(a * x + b, 0)
-    solution = Eq(x, -1 * Rational(b, a))
-
-    return {
-        "description": "Solve a linear equation.",
-        "question_latex": latex(question),
-        "solution_latex": latex(solution),
-    }
+    return question
