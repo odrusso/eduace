f2faaeeeaa9ad52b486c36fd5e73bdb40234517b
Oscar Russo <odrusso1@gmail.com>
2021-09-05 12:54:54 +1200

    And another test

--- web/src/components/tests/Question.test.tsx
+++ web/src/components/tests/Question.test.tsx
@@ -11,11 +11,22 @@ describe("question page", () => {
         await screen.findByText("selected question:")
     })
 
-    it("can select a ", async () => {
+    it("can select a question and data loads", async () => {
         renderAppWithRoute("/question")
         await screen.findByText("selected question:")
         const selectBox = screen.getByText("-- select an option --")
         userEvent.selectOptions(selectBox.parentElement, ["0"])
         await screen.findByText("selected question: mcat 1")
+    })
+
+    it("loads list of questions from API", async () => {
+        renderAppWithRoute("/question")
+        await screen.findByText("selected question:")
+        const selectBox = screen.getByText("-- select an option --")
+        expect(selectBox.parentElement.children.length).toBe(4)
+        screen.getByText("mcat 1")
+        screen.getByText("mcat 2")
+        screen.getByText("mcat 3")
+
     })
 })
