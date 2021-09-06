1d294741bb11f68c8207a6246530df7ca143b902
Ashton Moore <ashton.chris.moore@gmail.com>
2021-09-06 19:51:51 +1200

    handle posts back to question url with question attempt in body

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from .config import API_VERSION
 from time import time
 from .questions import get_question, get_all_questions
-from .solutions import get_solution
+from .solutions import check_solution
 
 app = Flask(__name__)
 
@@ -14,20 +14,20 @@ def question_router(question_type, question_id):
 
     return question.json, status
 
+@app.route(API_VERSION + "/question/<question_type>/<question_id>", methods=["POST"])
+def get_solution(question_type, question_id):
+
+    attempt = request.get_json(force=True)
+    attempt_response, status = check_solution(question_type, question_id, attempt)
+
+    return attempt_response.json, status
+
 @app.route(API_VERSION + "/questions", methods=["GET"])
 def questions():
     
     question_dict, status = get_all_questions()
 
     return question_dict, status
-
-@app.route(API_VERSION + "/solution/<question_type>/<question_id>", methods=["POST"])
-def solution_router(question_type, question_id):
-
-    seed = request.args.get("seed", time())
-    solution, status = get_solution(question_type, question_id, seed)
-
-    return solution.json, status
 
 if __name__ == "__main__":
     app.run(debug=True)
