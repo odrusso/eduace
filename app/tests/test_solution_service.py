b0265f7fb103c4f90a3793200ed179f6eff76ccc
Ashton Moore <ashton.chris.moore@gmail.com>
2021-09-06 22:36:43 +1200

    test solution_service

---
+++ app/tests/test_solution_service.py
@@ -0,0 +1,25 @@
+import unittest
+from app import solution_service
+
+
+class SolutionServiceTests(unittest.TestCase):
+
+    ## The following tests will need to be improved as is_correct
+    ## grows in complexity
+    def test_is_correct_correct(self):
+
+        question = "2 x + 3 = 0"
+        attempt = "x = -\\frac{3}{2}"
+
+        result = solution_service.is_correct(question, attempt, 'mcat', '1')
+
+        self.assertTrue(result)
+
+    def test_is_correct_correct(self):
+
+        question = "2 x + 3 = 0"
+        attempt = "x = 1000"
+
+        result = solution_service.is_correct(question, attempt, 'mcat', '1')
+
+        self.assertFalse(result)
