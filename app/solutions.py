3ed572a925518cee315b13653db8adb5aa057fd8
Ashton Moore <ashton.chris.moore@gmail.com>
2021-09-06 19:51:12 +1200

    attempt to implement attempts

--- app/solutions.py
+++ app/solutions.py
@@ -1,7 +1,8 @@
-from sympy import symbols, latex, Eq, Rational
+from .solution_service import is_correct
+from .questions import QUESTION_MAPPING
 
 
-class SolutionError(Exception):
+class AttemptError(Exception):
     def __init__(self):
         self.description = "Solution or question not found."
 
@@ -12,7 +13,7 @@ class SolutionError(Exception):
         }
 
 
-class SolutionTypeError(Exception):
+class AttemptTypeError(Exception):
     def __init__(self):
         self.description = "Solution or question type not found."
 
@@ -23,50 +24,34 @@ class SolutionTypeError(Exception):
         }
 
 
-class Solution:
-    def __init__(self, seed):
-        self.description = ""
-        self.solution = []
-        self.seed = seed
+class Attempt:
+    def __init__(self, question, attempt):
+        self.question = question
+        self.attempt = attempt
     
     @property
     def json(self):
         return {
-            "description": self.description,
-            "solution": self.solution
+            "question": self.question,
+            "attempt": self.attempt,
+            "result": is_correct(self.question, self.attempt),
         }
 
 
-class MCATSolution1(Solution):
-    def __init__(self, seed):
-        super().__init__(seed)
-        # TODO: write services class to handle coefficient generation in reproduceable way
-        x = symbols('x')
-        a = 2
-        b = 3
+def check_solution(question_type, question_id, attempt):
 
-        self.description = "Too easy"
-        self.solution = [latex(Eq(x, -1 * Rational(b, a)))]
+    question = attempt.get("question")
+    attempt = attempt.get("attempt")
 
-
-SOLUTION_MAPPING = {
-    'mcat': {
-        '1': MCATSolution1,
-    },
-}
-
-
-def get_solution(question_type, question_id, seed):
-
-    if question_type := SOLUTION_MAPPING.get(question_type, None):
-        if question := question_type.get(question_id, None):
-            solution = question(seed)
+    if question_type := QUESTION_MAPPING.get(question_type, None):
+        if question_type.get(question_id, None):
+            attempt_response = Attempt(question, attempt)
             status = 200
         else:
-            solution = SolutionError()
+            attempt_response = AttemptError()
             status = 404
     else:
-        solution = SolutionTypeError()
+        attempt_response = AttemptTypeError()
         status = 404
 
-    return solution, status
+    return attempt_response, status
