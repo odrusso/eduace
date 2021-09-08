e767db46b1367ac60173e2e9bd96a4cd33dd009c
Ashton Moore <ashton.chris.moore@gmail.com>
2021-09-08 21:15:18 +1200

    -2000

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ import os
 API_VERSION = "/api/v1"
 
 DEBUG = os.environ.get("FLASK_DEBUG", "False") == "True"
-CORS_ALLOWED_ORIGINS = "https://localhost:5000" if DEBUG else ""
+CORS_ALLOWED_ORIGINS = "https://localhost:3000" if DEBUG else ""
