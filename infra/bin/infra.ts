b8c72047e143ec1c87fec456c7158a5210c67d97
Oscar Russo <odrusso1@gmail.com>
2021-11-06 22:09:57 +1300

    chore: trigger infra ci run

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -5,5 +5,8 @@ import {App} from "@aws-cdk/core";
 
 const app = new App();
 new EduaceWebApp(app, 'InfraStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  },
 });
