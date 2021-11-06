2e71a5b4d4a53cc634be408308adb0ed19725e1b
Oscar Russo <odrusso1@gmail.com>
2021-11-06 22:13:01 +1300

    chore: undo previous chage

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -5,8 +5,5 @@ import {App} from "@aws-cdk/core";
 
 const app = new App();
 new EduaceWebApp(app, 'InfraStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
