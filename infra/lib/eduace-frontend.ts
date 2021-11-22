c7facba3c1b200c326f92ccfd84b47136229f9c3
Oscar Russo <odrusso1@gmail.com>
2021-11-23 08:38:45 +1300

    infra: add frontend cdk

---
+++ infra/lib/eduace-frontend.ts
@@ -0,0 +1,99 @@
+import {Aws, CfnOutput, Construct, Stack} from "@aws-cdk/core";
+import {Bucket} from "@aws-cdk/aws-s3";
+import {ARecord, HostedZone, RecordTarget} from "@aws-cdk/aws-route53";
+import {DnsValidatedCertificate} from "@aws-cdk/aws-certificatemanager";
+import {
+    CloudFrontAllowedMethods,
+    CloudFrontWebDistribution,
+    OriginAccessIdentity,
+    SecurityPolicyProtocol,
+    SSLMethod,
+    ViewerCertificate
+} from "@aws-cdk/aws-cloudfront";
+import {Metric} from "@aws-cdk/aws-cloudwatch";
+import {CanonicalUserPrincipal, PolicyStatement} from "@aws-cdk/aws-iam";
+import {CloudFrontTarget} from "@aws-cdk/aws-route53-targets";
+
+export class EduaceFrontend extends Construct {
+    constructor(scope: Stack, id: string, domainName: string) {
+        super(scope, id);
+
+        const frontendBucket = new Bucket(this, "FrontendBucket", {
+            bucketName: domainName,
+            websiteIndexDocument: "index.html",
+            websiteErrorDocument: "error.html",
+            publicReadAccess: false,
+        })
+
+        new CfnOutput(this, "EduaceFrontendSiteBucketARN", {
+            exportName: "S3FrontendCodeBucketARN",
+            value: frontendBucket.bucketArn
+        })
+
+        const cloudfrontOAI = new OriginAccessIdentity(this, 'FrontendCFOAI', {
+            comment: `OAI for ${id}`
+        });
+
+        frontendBucket.addToResourcePolicy(new PolicyStatement({
+            actions: ['s3:GetObject'],
+            resources: [frontendBucket.arnForObjects('*')],
+            principals: [new CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+        }));
+
+        const hostedZone = HostedZone.fromLookup(this, "EduaceZone", {
+            domainName: domainName
+        })
+
+        const tlsCertificate = new DnsValidatedCertificate(this, "EduaceFrontendCertificate", {
+            domainName: domainName,
+            hostedZone: hostedZone,
+            region: "us-east-1", // Cloudfront only checks this region for certificates.
+        })
+
+        const viewerTlsCertificate = ViewerCertificate.fromAcmCertificate({
+            certificateArn: tlsCertificate.certificateArn,
+            env: {
+                region: Aws.REGION,
+                account: Aws.ACCOUNT_ID
+            },
+            node: this.node,
+            stack: scope,
+            metricDaysToExpiry: (props) => {
+                return new Metric({
+                    namespace: "TLS Viewer Certificate Validity",
+                    metricName: "TLS Viewer Certificate Expired",
+                })
+            }
+        }, {
+            sslMethod: SSLMethod.SNI,
+            securityPolicy: SecurityPolicyProtocol.TLS_V1_1_2016,
+            aliases: [domainName]
+        })
+
+        const cloudfrontDistribution = new CloudFrontWebDistribution(this, "EduaceCloudfrontDistribution", {
+            viewerCertificate: viewerTlsCertificate,
+            originConfigs: [{
+                s3OriginSource: {
+                    s3BucketSource: frontendBucket,
+                    originAccessIdentity: cloudfrontOAI
+                },
+                behaviors: [{
+                    isDefaultBehavior: true,
+                    compress: true,
+                    allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS
+                }]
+            }]
+        });
+
+        new CfnOutput(this, "EduaceFrontendDistributionURL", {
+            exportName: "EduaceFrontendDistributionURL",
+            value: cloudfrontDistribution.distributionDomainName
+        })
+
+        new ARecord(this, "EduaceFrontendARecord", {
+            recordName: domainName,
+            target: RecordTarget.fromAlias(new CloudFrontTarget(cloudfrontDistribution)),
+            zone: hostedZone
+        })
+    }
+}
--- infra/lib/eduace-web-app.ts
+++ infra/lib/eduace-web-app.ts
@@ -1,10 +1,12 @@
 import {EduaceBackend} from "./eduace-backend";
 import {Construct, Stack, StackProps} from "@aws-cdk/core";
+import {EduaceFrontend} from "./eduace-frontend";
 
 export class EduaceWebApp extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     new EduaceBackend(this, "EduaceBackend")
+    new EduaceFrontend(this, "EduaceFrontend", "eduace.net")
   }
 }
