6cb0c5623c2225bd8477f8e10b7ab787086ac581
Oscar Russo <odrusso1@gmail.com>
2022-01-09 11:14:44 +1300

    Undo some eroneous change

--- app/api/helpers/config.py
+++ app/api/helpers/config.py
@@ -4,6 +4,6 @@ import os
 API_VERSION = "/api/v1"
 
 DEBUG = os.environ.get("FLASK_DEBUG", "False") == "True"
-CORS_ALLOWED_ORIGINS = "*"
+CORS_ALLOWED_ORIGINS = "http://localhost:8080" if DEBUG else "https://eduace.net"
 PORT = int(os.environ.get("FLASK_PORT", 80))
 GIT_HASH = os.environ.get("GIT_HASH_VERSION", "")
